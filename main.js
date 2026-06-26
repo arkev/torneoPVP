ef01745c2bc126ba82100b54152e5140307b08fa
Allen Kevin Zapien Zamora <yosoy@arkev.com>
2026-06-25 22:31:09 -0600

    actualizar resultados

--- main.js
+++ main.js
@@ -308,14 +308,14 @@ const PLAYERS = [
 /* Matriz de resultados Round-robin. "W"=Victoria Limpia(3), "CW"=Victoria Cerrada(2), "CL"=Derrota Cerrada(1), "L"=Derrota Limpia(0), null = mismo jugador */
 const RR_RESULTS = [
     //  alonsopf89     Arkev77     DizzyJuanito19     Hecvas11     LaloCasas     PitterCruise     Arlekcking     Maxiatico
-    [null, "L", "L", "", "L", "", "L", "L"],  // alonsopf89
-    ["W", null, "W", "W", "L", "CW", "L", ""],  // Arkev77
+    [null, "L", "L", "L", "L", "", "L", "L"],  // alonsopf89
+    ["W", null, "W", "W", "L", "CW", "L", "W"],  // Arkev77
     ["W", "L", null, "CW", "L", "", "CW", ""],  // DizzyJuanito19
-    ["", "L", "CL", null, "", "", "L", ""],  // Hecvas11
-    ["W", "W", "W", "", null, "W", "W", "W"],  // LaloCasas
+    ["W", "L", "CL", null, "CL", "", "L", ""],  // Hecvas11
+    ["W", "W", "W", "CW", null, "W", "W", "W"],  // LaloCasas
     ["", "CL", "", "", "L", null, "L", ""],  // PitterCruise
     ["W", "W", "CL", "W", "L", "W", null, ""],  // Arlekcking
-    ["W", "", "", "", "L", "", "", null]   // Maxiatico
+    ["W", "L", "", "", "L", "", "", null]   // Maxiatico
 ];
 
 /* ============================================================
